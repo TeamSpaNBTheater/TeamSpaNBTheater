18f5490c4657683e6d2e21fd1c8a1b786fbf51de
“handaewon” <handaewon17@gmail.com>
2024-01-18 07:12:50 +0900

    "feature/LogInVC0118”

--- TeamSpaNBTheater/Login/LogInVC.swift
+++ TeamSpaNBTheater/Login/LogInVC.swift
@@ -8,31 +8,26 @@ import UIKit
 
 class LogInVC: UIViewController {
 
-    @IBOutlet weak var id: UITextField!
+    @IBOutlet weak var userId: UITextField!
     
     @IBOutlet weak var password: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 비밀번호 보안 설정
+        password.isSecureTextEntry = true
 
       
     }
  
     @IBAction func loginButton(_ sender: Any) {
+        
     }
     
     @IBAction func signupButton(_ sender: Any) {
         
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
--- TeamSpaNBTheater/Login/SignUpVC.swift
+++ TeamSpaNBTheater/Login/SignUpVC.swift
@@ -8,30 +8,21 @@ import UIKit
 
 class SignUpVC: UIViewController {
     
-    @IBOutlet weak var id: UITextField!
+
+    @IBOutlet weak var userId: UITextField!
     
     @IBOutlet weak var password: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func signUpButton(_ sender: Any) {
-        UserDefaults.standard.set(id.text, forKey: "id")
+        
+        UserDefaults.standard.set(userId.text, forKey: "userId")
         
         UserDefaults.standard.set(password.text, forKey: "password")
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
 }
